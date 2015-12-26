5f146bb51a2e1663c25dea3a68997611a455c1d8
Bach Le <bach@bullno1.com>
2015-12-26 20:29:20 +0800

    Allow reallocation in allocator

--- src/lip/allocator.c
+++ src/lip/allocator.c
@@ -1,10 +1,14 @@
 #include "allocator.h"
 
-void* lip_crt_malloc(void* context, size_t size) { return malloc(size); }
+void* lip_crt_realloc(void* context, void* old, size_t size)
+{
+	return realloc(old, size);
+}
+
 void lip_crt_free(void* context, void* mem) { free(mem); }
 
 static lip_allocator_t lip_crt_allocator = {
-	lip_crt_malloc, lip_crt_free
+	lip_crt_realloc, lip_crt_free
 };
 
 lip_allocator_t* lip_default_allocator = &lip_crt_allocator;
--- src/lip/allocator.h
+++ src/lip/allocator.h
@@ -5,13 +5,14 @@
 
 typedef struct lip_allocator_t
 {
-	void*(*malloc)(void* self, size_t size);
+	void*(*realloc)(void* self, void* old, size_t size);
 	void(*free)(void* self, void* mem);
 } lip_allocator_t;
 
 #define LIP_NEW(ALLOCATOR, TYPE) (TYPE*)(LIP_MALLOC(ALLOCATOR, sizeof(TYPE)))
 #define LIP_DELETE LIP_FREE
-#define LIP_MALLOC(ALLOCATOR, SIZE) (ALLOCATOR->malloc(ALLOCATOR, SIZE))
+#define LIP_MALLOC(ALLOCATOR, SIZE) LIP_REALLOC(ALLOCATOR, 0, SIZE)
+#define LIP_REALLOC(ALLOCATOR, PTR, SIZE) (ALLOCATOR->realloc(ALLOCATOR, PTR, SIZE))
 #define LIP_FREE(ALLOCATOR, MEM) (ALLOCATOR->free(ALLOCATOR, MEM))
 
 extern lip_allocator_t* lip_default_allocator;
