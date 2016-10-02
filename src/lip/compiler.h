b0f9a70e761110d8c20cd347c8b59df3dd316c96
Bach Le <bach@bullno1.com>
2016-10-02 13:17:39 +0800

    Clean up

--- src/lip/compiler.h
+++ src/lip/compiler.h
@@ -15,11 +15,6 @@ lip_compiler_destroy(lip_compiler_t* compiler);
 void
 lip_compiler_begin(lip_compiler_t* compiler, lip_string_ref_t source_name);
 
-void
-lip_compiler_add_ast_transform(
-	lip_compiler_t* compiler, lip_ast_transform_t* transform
-);
-
 const lip_error_t*
 lip_compiler_add_sexp(lip_compiler_t* compiler, const lip_sexp_t* sexp);
 
