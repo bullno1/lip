2e18e168bb3c74ae90987a9d95d78c1e6a757fa7
Bach Le <bach@bullno1.com>
2016-12-26 16:02:29 +0800

    Add builtin function: print

--- src/lip/builtins.c
+++ src/lip/builtins.c
@@ -1,4 +1,6 @@
 #include "lip.h"
+#include "print.h"
+#include "io.h"
 
 static lip_exec_status_t
 lip_nop(lip_vm_t* vm)
@@ -19,11 +21,25 @@ lip_identity(lip_vm_t* vm)
 	return LIP_EXEC_OK;
 }
 
+static lip_exec_status_t
+lip_print(lip_vm_t* vm)
+{
+	(void)vm;
+	uint8_t argc;
+	lip_value_t* argv = lip_vm_get_args(vm, &argc);
+	if(argc != 1) { return LIP_EXEC_ERROR; }
+	lip_print_value(3, 0, lip_stdout(), argv[0]);
+	lip_vm_push_value(vm, argv[0]);
+
+	return LIP_EXEC_OK;
+}
+
 void
 lip_load_builtins(lip_context_t* ctx)
 {
 	lip_ns_context_t* ns = lip_begin_ns(ctx, lip_string_ref(""));
 	lip_declare_function(ns, lip_string_ref("nop"), lip_nop);
 	lip_declare_function(ns, lip_string_ref("identity"), lip_identity);
+	lip_declare_function(ns, lip_string_ref("print"), lip_print);
 	lip_end_ns(ctx, ns);
 }
