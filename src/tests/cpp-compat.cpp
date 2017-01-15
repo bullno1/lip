1d4f3f443c9f4374f8fe5070499956e66152daab
Bach Le <bach@bullno1.com>
2017-01-15 17:34:45 +0800

    Add test for binding C++ functions to lip

--- src/tests/cpp-compat.cpp
+++ src/tests/cpp-compat.cpp
@@ -16,9 +16,36 @@
 #include <lip/sexp.h>
 #include <lip/vm.h>
 #include <lip/pp.h>
+#include <lip/extra.h>
+
+#define _Bool bool
+#include "munit.h"
+
+namespace cpp {
+
+lip_exec_status_t cpp_identity(lip_vm_t* vm, lip_value_t* result)
+{
+	lip_bind_args((any, x));
+	lip_return(x);
+}
+
+}
 
 extern "C" void test_cpp()
 {
 	lip_runtime_t* runtime = lip_create_runtime(lip_default_allocator);
+	lip_context_t* ctx = lip_create_context(runtime, NULL);
+	lip_vm_config_t vm_config;
+	vm_config.env_len = 512;
+	vm_config.cs_len = 512;
+	vm_config.os_len = 512;
+	lip_vm_t* vm = lip_create_vm(ctx, &vm_config);
+	lip_value_t fn = lip_make_function(vm, cpp::cpp_identity, 0, NULL);
+	lip_value_t result;
+	lip_exec_status_t status = lip_call(vm, &result, fn, 1, lip_make_number(vm, 42.5));
+	munit_assert_uint(LIP_EXEC_OK, ==, status);
+	munit_assert_uint(LIP_VAL_NUMBER, ==, result.type);
+	munit_assert_double_equal(42.5, result.data.number, 2);
+
 	lip_destroy_runtime(runtime);
 }
